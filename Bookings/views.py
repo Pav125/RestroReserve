509b882c8276e22211e5203a1e36c923d43514cb
chaitanya1270 <nagachaitu1270@gmail.com>
2024-06-26 15:27:31 +0530

    added status

--- Bookings/views.py
+++ Bookings/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def status_table(request):
+    tables = [
+        {'name': 'Table1', 'available': False},
+        {'name': 'Table2', 'available': True},
+        {'name': 'Table3', 'available': False},
+        {'name': 'Table4', 'available': True},
+        {'name': 'Table5', 'available': False},
+        {'name': 'Table6', 'available': True},
+    ]
+    context = {
+        'tables' : tables
+    }
+    return render(request, 'Bookings/status_table.html', context)
