2847be528d190414fbb4d953c392b126464a677c
N-PrasanthKumar <135829072+N-PrasanthKumar@users.noreply.github.com>
2024-06-26 14:27:56 +0530

    home.html

--- Bookings/views.py
+++ Bookings/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "Bookings/base.html")
+    return render(request, "Bookings/home.html")
